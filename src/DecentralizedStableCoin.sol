57e1442768ac20c37aa601b0eabfbd1adad46c7e
Bohr <bohrplanet@gmail.com>
2023-08-16 09:08:22 -0700

    add burn function for DSC contract

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -23,6 +23,7 @@
 pragma solidity ^0.8.18;
 
 import {ERC20Burnable, ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 /*
  * @title DecentralizedStableCoin
@@ -31,10 +32,26 @@ import {ERC20Burnable, ERC20} from "../lib/openzeppelin-contracts/contracts/toke
  * Minting: Algorithmic
  * Relative Stablility: Pegged to USD
  *
- * This is the contract meant to be governed by ESCEngine. This contract is just ERC20
+ * @dev This is the contract meant to be governed by ESCEngine. This contract is just ERC20
  * implementation of our stablecoin system.
  *
  */
-contract DecentralizedStableCoin is ERC20Burnable {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+
     constructor() ERC20("DecentralizedStableCoin", "DSC") {}
+
+    // Will use DSCEngine contract to deploy this contract, so onlyOwner means only DSCEngine can call this function
+    // this makes this stablecoin system fully decentralized
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount);
+    }
 }
